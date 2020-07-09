ec49eec878e3edad6bcb942e223cd5f29aa62ddc
KarthikSubramani910 <64369524+KarthikSubramani910@users.noreply.github.com>
2020-07-10 00:13:19 +0530

    AuthChanges

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,10 +3,14 @@ import { Injectable } from '@angular/core';
 import { Subject, Observable } from 'rxjs';
 @Injectable()
 export class AuthService {
+  loggedIn;
   signedIn: Subject<boolean> = new Subject();
   constructor(private route: Router) {}
 
-  authenticatedSubject(): Observable<boolean> {
-    return this.signedIn;
+  authenticatedSubject() {
+    this.signedIn.subscribe((login: boolean) => {
+      this.loggedIn = login;
+    });
+    return this.loggedIn;
   }
 }
